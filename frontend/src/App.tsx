27b42d5ae6eb772e0702ecb9934f90c038f6218b
Timothy Estrada <tim_1227@aol.com>
2023-03-17 19:33:38 +0000

    Added routes to new Pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,8 @@ import './App.css';
 import { Routes, Route, CompatRouter } from 'react-router-dom-v5-compat';
 import CoveyTownSplash from './pages/Home';
 import AppStateWrapper from './pages/Town';
+import Register from './pages/Register';
+import Login from './pages/Login';
 
 export default function App() {
   return (
@@ -12,6 +14,8 @@ export default function App() {
           <Routes>
             <Route path='/' element={<CoveyTownSplash />} />
             <Route path='/Town' element={<AppStateWrapper />} />
+            <Route path='/Login' element={<Login />} />
+            <Route path='/Register' element={<Register />} />
           </Routes>
         </CompatRouter>
       </main>
