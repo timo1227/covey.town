59351058f3b8191cc99ebdc60c5d9b05c3088ade
Tim <tim_1227@aol.com>
2023-03-18 00:14:38 -0400

    Linked Sections to Nav

--- frontend/src/components/LandingPage/navBar.tsx
+++ frontend/src/components/LandingPage/navBar.tsx
@@ -7,8 +7,8 @@ import logo from '../../public/logo.png';
 const NAVIGATION = [
   { name: 'Home', href: '/' },
   { name: 'Town', href: '/Town' },
-  { name: 'Features', href: '#' },
-  { name: 'About', href: '#About' },
+  { name: 'Features', href: '/#Features' },
+  { name: 'About', href: '/#About' },
 ];
 
 export default function Nav() {
@@ -53,7 +53,7 @@ export default function Nav() {
         <Dialog.Panel className='fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10'>
           <div className='flex items-center justify-between'>
             <a href='#' className='-m-1.5 p-1.5'>
-              <span className='sr-only'>Your Company</span>
+              <span className='sr-only'>Covey Town</span>
               <img className='h-8 w-auto' src={logo} alt='Covey Town Logo' />
             </a>
             <button
