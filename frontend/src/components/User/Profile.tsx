052cdb685949664cc86c020dc27a761f189a2484
Tim <tim_1227@aol.com>
2023-05-03 22:42:42 -0400

    Style

--- frontend/src/components/User/Profile.tsx
+++ frontend/src/components/User/Profile.tsx
@@ -86,7 +86,7 @@ export default function Profile() {
         Edit Profile
       </h2>
       {error && (
-        <div className='bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative'>
+        <div className='bg-red-100 border border-red-400 text-red-700 px-4 py-3 pr-16 rounded relative'>
           <strong className='font-bold'>Error!</strong>
           <span className='block sm:inline'> {error}</span>
           <span className='absolute top-0 bottom-0 right-0 px-4 py-3'>
